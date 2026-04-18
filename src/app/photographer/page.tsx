6441d86534320250ca3c7651ab045eeb33a3b29b
nltdat <nltdat.it@gmail.com>
2026-04-19 01:35:31 +0700

    FE-1 home page

--- src/app/photographer/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PhotographerDashboardPage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-3xl font-semibold text-neutral-900">
-        Photographer Dashboard
-      </h1>
-    </main>
-  );
-}
